f573bf580f565e064f99ae96eb4eae75807593b4
AjayGhimire <ajayghimire1998@gmail.com>
2022-03-29 17:54:10 +1100

    done with the lab

--- src/components/App.js
+++ src/components/App.js
@@ -4,17 +4,23 @@ import itemData from "../data/items";
 
 function App() {
   const [items, setItems] = useState(itemData);
+  const [isDark, setIsDark] = useState(false);
+
+  function ToggleMode(){
+    setIsDark((isDark) => !isDark)
+  }
+
 
   // this data will be passed down to the ShoppingList as a prop
   console.log(items);
 
   return (
-    <div className={"App " + (false ? "dark" : "light")}>
+    <div className={"App " + (isDark? "dark" : "light")}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={ToggleMode}>{isDark ? "Light Mode": "Dark Mode"}</button>
       </header>
-      <ShoppingList items={items} />
+      <ShoppingList items={items} category = {items.category}/>
     </div>
   );
 }
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,18 @@
 import React from "react";
+import { useState } from "react/cjs/react.development";
 
 function Item({ name, category }) {
+
+  const[inCart, setInCart] = useState(false);
+
+  function handleCart() {
+    setInCart((inCart) => !inCart);
+  }  
   return (
-    <li className="">
+    <li className={inCart? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={inCart? "in-cart" : "add"} onClick={handleCart}>{inCart? "Remove from Cart": "Add to Cart"}</button>
     </li>
   );
 }
